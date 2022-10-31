8320923948d87e307b9a298b68e74841175e766d
mao <1296193245@qq.com>
2022-10-31 19:22:41 +0800

    springboot_logback_demo

---
+++ src/main/java/mao/springboot_logback_demo/SpringbootLogbackDemoApplication.java
@@ -0,0 +1,15 @@
+package mao.springboot_logback_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLogbackDemoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringbootLogbackDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/springboot_logback_demo/SpringbootLogbackDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.springboot_logback_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLogbackDemoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
